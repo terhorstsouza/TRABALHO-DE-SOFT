4bc396124f55b08e119426d54451ead2e5e45245
Gabriel Souza <gterhorsts@gmail.com>
2018-04-25 23:04:52 -0300

    correção de erros

--- PARTE-1-GABRIEL.py
+++ PARTE-1-GABRIEL.py
@@ -3,11 +3,12 @@ valores = 0
 import json
 
 
+
 with open ('Arquivo.txt','r') as arquivo:
     conteudo = arquivo.read()
     estoque = json.loads(conteudo)
 
-#HUB de escolhas
+
 print('O que deseja fazer?')
 print('A - Adicionar loja')
 print('B - Acessar loja')
@@ -16,29 +17,29 @@ print('C - Excluir loja')
 escolha2 = input('Digite sua escolha:  ')
 
 if escolha2 == 'A':
-    Franquia = {}
+    
     Nome = input('Digite sua loja: ')
-    while Nome in Franquia:
+    while Nome in estoque:
         print('Já está Cadastrada')
         Nome = input('Digite sua loja: ')
         
 if escolha2 == 'B':
-    Franquia = {}
+    
     Nome = input('Digite sua loja: ')
-    if Nome in Franquia:
-        print(Franquia[Nome])
+    if Nome in estoque:
+        print(estoque[Nome])
 
     else:
-        while Nome not in Franquia:    
-            print('Essa loja não existe')
+        print('Essa loja não existe')
+	while Nome not in estoque:    
             Nome = input('Digite sua loja: ')
 
 if escolha2 == 'C':
-    Franquia = {}
+    
     Nome = input('Digite sua loja: ')
-    if Nome not in Franquia:
+    if Nome not in estoque:
         print('Essa loja não existe')
-        while Nome not in Franquia:
+        while Nome not in estoque:
            Nome = input('Digite sua loja: ')
 
 print("Controle de Estoque")
@@ -50,48 +51,53 @@ print("0 - sair",
       "5 - Alterar preço")
 
 escolha = int(input("Faça sua escolha: "))
-estoque = {}
+
 
 
 
 while escolha > 0:        
     if escolha == 1:
         nome_produto = input('Digite o Produto: ')
-        if nome_produto in Franquia[Nome]:
+        if nome_produto in estoque[Nome]:
             print('Esse produto já está no estoque')
             
         quantidade = int(input('Digite a quantidade do produto: '))
         if quantidade < 0:
             print('A quantidade inicial não pode ser negativa')
-               
-        preco = int(input('Digite o preço do produto: '))
-        Franquia[Nome][nome_produto] = {}
-        Franquia[Nome][nome_produto]['Quantidade'] = quantidade
-        Franquia[Nome][nome_produto]['preco'] = preco
+            break 
+        preco = float(input('Digite o preço do produto: '))
+	if preco < 0:
+	    print('A quantidade inicial não pode ser negativa')
+	    break
+        estoque[Nome][nome_produto] = {}
+        estoque[Nome][nome_produto]['Quantidade'] = quantidade
+        estoque[Nome][nome_produto]['preco'] = preco
 
     if escolha == 2:
         nome_produto = input('Digite o Produto: ')
-        if nome_produto in Franquia[Nome]:
-            del Franquia[Nome][nome_produto]
+        if nome_produto in estoque[Nome]:
+            del estoque[Nome][nome_produto]
         else:
             print('Nao se encontra no estoque')
             
 
     if escolha == 3:
          nome_produto = input('Digite o Produto: ')
-         if nome_produto in Franquia[Nome]:
-            quantidade+= int(input('Digite a quantidade do produto adicionada/removida: '))
-         else:
+         if nome_produto in estoque[Nome]:
+            nova_quantidade+= (int(input('Digite a quantidade do produto adicionada/removida: ')))
+            estoque[Nome][nome_produto]['Quantidade'] += nova_quantidade 
+	 else:
             print("Esse produto não está no estoque")
-         Franquia[Nome][nome_produto] = quantidade
+	    
+         
     
     if escolha == 4:
-        print(Franquia)
+        print(estoque[Nome])
     
     if escolha == 5:
-        nome_produto = int(input('Digite um produto: '))
-        novo_preco = int(input('Digite o novo preco: '))
-        estoque[nome_produto]['preco'] = novo_preco
+        nome_produto = input('Digite um produto: ')
+        novo_preco = float(input('Digite o novo preco: '))
+        estoque[Nome][nome_produto]['preco'] = novo_preco
     
 
     print("Controle de Estoque")
@@ -110,19 +116,18 @@ if escolha == 0:
    
     print("até mais!")
 
-
-    for coisas in Franquia[Nome]:
-    	valor = Franquia[Nome][coisas]['Quantidade']*Franquia[Nome][coisas]['preco']
-    	valores += valor
-    print('O valor total em estoque é: {0} Reais'.format(valores))
+for coisas in estoque[Nome]:
+    valor = (int(estoque[Nome][coisas]['Quantidade']))*(int(estoque[Nome][coisas]['preco']))
+    valores += valor
+print('O valor total em estoque é: {0} Reais'.format(valores))
 
 
     
-    for produtos in estoque:
-        if (estoque[produtos]['Quantidade']) < 0:
-            negativos.append(produtos)
-    print('Os seguintes produtos possuem estoques negativos: ')
-    print(negativos)
+for produtos in estoque[Nome]:
+    if (estoque[Nome][produtos]['Quantidade']) < 0:
+       	negativos.append(produtos)
+print('Os seguintes produtos possuem estoques negativos: ')
+print(negativos)
 
 
 
